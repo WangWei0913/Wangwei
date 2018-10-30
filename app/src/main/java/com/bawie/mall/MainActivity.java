1c97f96b5eca7554d859ea16d6d73b2fb2651c0b
herion_wang <2395763676@qq.com>
2018-10-30 14:30:49 +0800

    提交mainactivity

---
+++ app/src/main/java/com/bawie/mall/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bawie.mall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //此处有魔法1，勿动
+
+        //此处有魔法2，勿动
+
+    }
+}
